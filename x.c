0d955b412ce7c619cfa218b820d2681deb247a1d
Sergey Silaev <hello@8am.dev>
2019-12-04 15:20:08 +0300

    Right-click also paste from the selection

--- x.c
+++ x.c
@@ -654,7 +654,7 @@ brelease(XEvent *e)
 		return;
 	}
 
-	if (e->xbutton.button == Button2)
+	if (e->xbutton.button == Button2 || e->xbutton.button == Button3)
 		selpaste(NULL);
 	else if (e->xbutton.button == Button1)
 		mousesel(e, 1);
