5605f790e79b1672ccfbb8c97ea7d7c37b9eceea
Sergey Silaev <hello@8am.dev>
2023-05-30 22:36:09 +0200

    Apply patches from the original repo:
    - Add-support-for-DSR-response-OK-escape-sequence,
    - Fixed-OSC-color-reset-without-parameter-resets-all-c
    - ignore-C1-control-characters-in-UTF-8-mode

--- config.h
+++ config.h
@@ -100,7 +100,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 1.0;
 
 /* Apprentice colorscheme */
 /* Terminal colors (16 first used in escape sequence) */
